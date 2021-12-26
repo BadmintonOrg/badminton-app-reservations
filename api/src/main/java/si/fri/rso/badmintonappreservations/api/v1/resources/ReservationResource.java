66f159a00e83bbc0229a41feead8eb1728809a24
janezbozic <janez.bozic1@gmail.com>
2021-12-26 15:32:34 +0100

    Added logs and cors

--- api/src/main/java/si/fri/rso/badmintonappreservations/api/v1/resources/ReservationResource.java
+++ api/src/main/java/si/fri/rso/badmintonappreservations/api/v1/resources/ReservationResource.java
@@ -1,5 +1,6 @@
 package si.fri.rso.badmintonappreservations.api.v1.resources;
 
+import com.kumuluz.ee.cors.annotations.CrossOrigin;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.headers.Header;
@@ -26,6 +27,7 @@ import java.util.logging.Logger;
 @Path("/reservations")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@CrossOrigin(supportedMethods = "GET, POST, HEAD, DELETE, OPTIONS")
 public class ReservationResource {
 
     private Logger log = Logger.getLogger(ReservationResource.class.getName());
